8a0aedb800cb54972056820240ed1462ff720ad8
igopereira1 <igopereira1@gmail.com>
2024-06-23 17:35:45 -0300

    feat: add constructor to Address embeddable

--- MedicalManagement/src/main/java/docpat/met/api/model/Address.java
+++ MedicalManagement/src/main/java/docpat/met/api/model/Address.java
@@ -1,9 +1,16 @@
 package docpat.met.api.model;
 
+import docpat.met.api.dto.AddressDTO;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address {
 
     private String street;
@@ -14,4 +21,14 @@ public class Address {
     @Column(name = "zip_code")
     private String zipCode;
     private String complement;
+
+    public Address(AddressDTO addressData) {
+        this.street = addressData.street();
+        this.number = addressData.number();
+        this.neighborhood = addressData.neighborhood();
+        this.city = addressData.city();
+        this.state = addressData.state();
+        this.zipCode = addressData.zipCode();
+        this.complement = addressData.complement();
+    }
 }
