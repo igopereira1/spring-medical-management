c759b8a7d5b07bae79fa182da047752f9fd4d74a
igopereira1 <igopereira1@gmail.com>
2024-06-23 17:38:56 -0300

    feat: add POST endpoints to DoctorController and PatientController

--- MedicalManagement/src/main/java/docpat/met/api/controller/DoctorController.java
+++ MedicalManagement/src/main/java/docpat/met/api/controller/DoctorController.java
@@ -1,6 +1,11 @@
 package docpat.met.api.controller;
 
 import docpat.met.api.dto.DoctorDTO;
+import docpat.met.api.model.Doctor;
+import docpat.met.api.repository.DoctorRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("medicos")
 public class DoctorController {
+    @Autowired
+    DoctorRepository doctorRepository;
 
     @PostMapping
-    public void createDoctor(@RequestBody DoctorDTO doctor) {
-        System.out.println(doctor);
+    @Transactional
+    public void createDoctor(@RequestBody @Valid DoctorDTO doctor) {
+        doctorRepository.save(new Doctor(doctor));
     }
 }
--- MedicalManagement/src/main/java/docpat/met/api/controller/PatientController.java
+++ MedicalManagement/src/main/java/docpat/met/api/controller/PatientController.java
@@ -1,6 +1,12 @@
 package docpat.met.api.controller;
 
 import docpat.met.api.dto.PatientDTO;
+
+import docpat.met.api.model.Patient;
+import docpat.met.api.repository.PatientRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("pacientes")
 public class PatientController {
+    @Autowired
+    PatientRepository patientRepository;
 
     @PostMapping
-    public void createPatient(@RequestBody PatientDTO patient) {
-        System.out.println(patient);
+    @Transactional
+    public void createPatient(@RequestBody @Valid PatientDTO patient) {
+        patientRepository.save(new Patient(patient));
     }
 }
