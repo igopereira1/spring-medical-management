d068b2be72ec0a107599a643e4b524537a066e42
igopereira1 <igopereira1@gmail.com>
2024-06-24 22:29:04 -0300

    feat: add id field to DoctorListDTO and PatientListDTO for update operations

--- MedicalManagement/src/main/java/docpat/met/api/dto/DoctorListDTO.java
+++ MedicalManagement/src/main/java/docpat/met/api/dto/DoctorListDTO.java
@@ -2,9 +2,9 @@ package docpat.met.api.dto;
 
 import docpat.met.api.model.Doctor;
 
-public record DoctorListDTO(String name, String crm, String specialty, String email) {
+public record DoctorListDTO(Long id, String name, String crm, String specialty, String email) {
 
     public DoctorListDTO(Doctor doctor) {
-        this(doctor.getName(), doctor.getCrm(), doctor.getSpecialty().name(), doctor.getEmail());
+        this(doctor.getId(), doctor.getName(), doctor.getCrm(), doctor.getSpecialty().name(), doctor.getEmail());
     }
 }
--- MedicalManagement/src/main/java/docpat/met/api/dto/PatientListDTO.java
+++ MedicalManagement/src/main/java/docpat/met/api/dto/PatientListDTO.java
@@ -2,9 +2,9 @@ package docpat.met.api.dto;
 
 import docpat.met.api.model.Patient;
 
-public record PatientListDTO(String name, String cpf, String email) {
+public record PatientListDTO(Long id, String name, String cpf, String email) {
 
     public PatientListDTO(Patient patient) {
-        this(patient.getName(), patient.getCpf(), patient.getEmail());
+        this(patient.getId(), patient.getName(), patient.getCpf(), patient.getEmail());
     }
 }
